dcd77a3c54dc435a2bb1ef2a5a800e5856cc2516
罗海林 <luohailin@followme.cn>
2020-06-11 17:05:38 +0800

    feat: 优化日志输出

--- v1/log/log.go
+++ v1/log/log.go
@@ -4,40 +4,45 @@ import (
 	"github.com/RichardKnop/logging"
 	cron2 "github.com/robfig/cron"
 	"gopkg.in/natefinch/lumberjack.v2"
+	"sync"
 )
 
 var (
-	rollingLog = &lumberjack.Logger{
-		Filename:   GetLogFilePath("machinery"),
-		MaxSize:    0x1000 * 5, // automatic rolling file on it increment than 2GB,
-		MaxBackups: 60,         // reserve last 60 day logs
-		LocalTime:  true,
-		Compress:   true,
-	}
-
-	logger = logging.New(rollingLog, rollingLog, new(logging.ColouredFormatter))
-
-	// DEBUG ...
-	DEBUG = logger[logging.DEBUG]
-	// INFO ...
-	INFO = logger[logging.INFO]
-	// WARNING ...
-	WARNING = logger[logging.WARNING]
-	// ERROR ...
-	ERROR = logger[logging.ERROR]
-	// FATAL ...
-	FATAL = logger[logging.FATAL]
+	once    sync.Once
+	WARNING logging.LoggerInterface
+	INFO    logging.LoggerInterface
+	ERROR   logging.LoggerInterface
+	FATAL   logging.LoggerInterface
+	DEBUG   logging.LoggerInterface
 )
 
 func init() {
-	cron := cron2.New()
-	err := cron.AddFunc("0 0 23 * * ?", func() {
-		_ = rollingLog.Rotate()
+	once.Do(func() {
+		rollingLog := &lumberjack.Logger{
+			Filename:   GetLogFilePath("machinery"),
+			MaxSize:    0x1000 * 5, // automatic rolling file on it increment than 2GB,
+			MaxBackups: 60,         // reserve last 60 day logs
+			LocalTime:  true,
+			Compress:   true,
+		}
+
+		logger := logging.New(rollingLog, rollingLog, new(logging.ColouredFormatter))
+
+		DEBUG = logger[logging.DEBUG]
+		INFO = logger[logging.INFO]
+		WARNING = logger[logging.WARNING]
+		ERROR = logger[logging.ERROR]
+		FATAL = logger[logging.FATAL]
+
+		cron := cron2.New()
+		err := cron.AddFunc("0 0 23 * * ?", func() {
+			_ = rollingLog.Rotate()
+		})
+		if err != nil {
+			panic(err)
+		}
+		cron.Start()
 	})
-	if err != nil {
-		panic(err)
-	}
-	cron.Start()
 }
 
 // Set sets a custom logger for all log levels
